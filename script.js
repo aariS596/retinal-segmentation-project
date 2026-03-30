74cb95c17630aa8d9fd11250ceb4d5dd8baa502d
Rahman Sarker <aarianasarker@gmail.com>
2026-03-29 23:19:27 -0400

    initial commit: retinal portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+function showDemo() {
+  document.getElementById("demoText").innerText =
+    "Segmentation executed successfully (simulated demo).";
+  document.getElementById("segmentedImg").classList.add("visible");
+}
